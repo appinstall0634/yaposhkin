06b972c864872bf539a11b6d545013b630a7121e
amantur <amantur.mamatazimov04@gmail.com>
2025-06-27 00:29:49 +0600

    commit

--- index.js
+++ index.js
@@ -656,8 +656,6 @@ async function calculateDeliveryAndSubmitOrder(phone_no_id, from, orderItems, to
                 console.log(`This is address lati2 ${address.geocoding_json.latitude}`);
                 console.log(`This is address longitude2 ${address.geocoding_json.longitude}`);
             }
-            const tempLat = address?.geocoding_json.latitude;
-            const tempLon = address?.geocoding_json.longitude;
             // Если есть координаты - рассчитываем доставку
             if (address?.geocoding_json?.latitude && address?.geocoding_json?.longitude) {
                 const lat = tempLat;
